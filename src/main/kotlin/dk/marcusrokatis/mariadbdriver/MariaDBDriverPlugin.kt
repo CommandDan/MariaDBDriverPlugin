6e2f78d8347ba752a4ebd3f5a214fe92088639f3
Marcus Rokatis <marcuskaer@gmail.com>
2025-11-13 19:17:11 +0100

    Initial commit

---
+++ src/main/kotlin/dk/marcusrokatis/mariadbdriver/MariaDBDriverPlugin.kt
@@ -0,0 +1,14 @@
+package dk.marcusrokatis.mariaDBDriverPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MariaDBDriverPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
